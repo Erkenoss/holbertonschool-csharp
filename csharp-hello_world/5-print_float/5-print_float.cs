336e79e53bcc722eaa19d4c810f4e921b8bb4134
Erkenoss <xavier.bertin6@gmail.com>
2024-04-09 12:18:13 +0200

    change syntaxe... Again

--- csharp-hello_world/5-print_float/5-print_float.cs
+++ csharp-hello_world/5-print_float/5-print_float.cs
@@ -2,9 +2,9 @@
 
 class Program
 {
-	static void Main(string[] args)
+	    static void Main(string[] args)
     {
-		float number = 3.14159f;
+        float number = 3.14159f;
         Console.WriteLine("Float: {0:N2}", number);
     }
 }
