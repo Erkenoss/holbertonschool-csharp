8851aca633b54a1e2b7a8b632fec17ce05c8c64f
Erkenoss <xavier.bertin6@gmail.com>
2024-04-09 17:16:17 +0200

    add task 5-

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i;
+        for (i = 0; i < 98; i++) {
+            Console.Write("{0}{1}, ", i/10, i%10);
+        }
+        Console.Write("{0}\n", i);
+    }
+}
