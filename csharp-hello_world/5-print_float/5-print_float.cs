420eccdfa46f8415e6347876f1321cf09d3c9f91
Erkenoss <xavier.bertin6@gmail.com>
2024-04-09 12:23:49 +0200

    fixed?

--- csharp-hello_world/5-print_float/5-print_float.cs
+++ csharp-hello_world/5-print_float/5-print_float.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿﻿using System;
 
 class Program
 {
-	    static void Main(string[] args)
+	static void Main(string[] args)
     {
         float number = 3.14159f;
         Console.WriteLine("Float: {0:N2}", number);
