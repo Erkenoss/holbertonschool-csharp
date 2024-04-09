de930bb4c270ca93be61eb52f2787c76799a53f4
Erkenoss <xavier.bertin6@gmail.com>
2024-04-09 17:46:50 +0200

    add task 6-

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i;
+        for (i = 0; i <= 99; i++) {
+            if (i/10 != i%10 && i%10 > i/10) {
+                if (i != 89) {
+                    Console.Write("{0}{1}, ", i/10, i%10);
+                }
+                else {
+                    Console.Write("{0}\n", i);
+                }
+            }
+        }
+    }
+}
