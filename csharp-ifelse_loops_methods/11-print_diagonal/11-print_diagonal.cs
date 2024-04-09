62c8b3bab9e8f3ac314e20af324fe9c2de72cf53
Erkenoss <xavier.bertin6@gmail.com>
2024-04-09 19:08:19 +0200

    add task 11-

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Line {
+    public static void PrintDiagonal(int length) {
+        if (length <= 0) {
+            Console.WriteLine();
+            return;
+        }
+
+        int space = 0;
+        int backSlash = 0;
+        while (space < length) {
+            while (backSlash < space) {
+                Console.Write(' ');
+                backSlash++;
+            }
+            Console.Write("\\\n");
+            space++;
+            backSlash = 0;
+        }
+    }
+}
