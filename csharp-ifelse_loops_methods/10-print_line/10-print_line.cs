b39da439d3be65fe15b3e57afaa6319f719da503
Erkenoss <xavier.bertin6@gmail.com>
2024-04-09 18:26:35 +0200

    add task 10-

---
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Line {
+    public static void PrintLine(int lenght) {
+        if (lenght <= 0) {
+            Console.WriteLine();
+            return;
+        }
+        for (int i = 0; i < lenght; i++) {
+            Console.Write('_');
+        }
+        Console.WriteLine();
+    }
+}
