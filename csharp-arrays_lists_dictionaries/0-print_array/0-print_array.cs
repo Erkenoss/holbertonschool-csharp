4a365486087c2e8a6b269f53ddec84413788843d
Erkenoss <xavier.bertin6@gmail.com>
2024-04-09 19:51:11 +0200

    add task 0-

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Array {
+    public static int[] CreatePrint(int size) {
+        if (size == 0) {
+            Console.WriteLine();
+            return new int[0];
+        }
+        if (size < 0) {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] newArray = new int[size];
+        for (int i = 0; i < size; i++) {
+            newArray[i] = i;
+            Console.Write(newArray[i] + " ");
+        }
+        Console.WriteLine();
+
+        return newArray;
+    }
+}
