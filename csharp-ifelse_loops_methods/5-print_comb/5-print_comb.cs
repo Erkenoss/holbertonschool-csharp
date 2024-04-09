60a91deef579f816fb18f30f4701dd7e18ccbad2
Erkenoss <xavier.bertin6@gmail.com>
2024-04-09 17:17:15 +0200

    i max + 1

--- csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         int i;
-        for (i = 0; i < 98; i++) {
+        for (i = 0; i < 99; i++) {
             Console.Write("{0}{1}, ", i/10, i%10);
         }
         Console.Write("{0}\n", i);
