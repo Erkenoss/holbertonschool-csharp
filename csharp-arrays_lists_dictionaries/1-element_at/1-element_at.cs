e4156ebbf0ef169297c944a85944218038565061
Erkenoss <xavier.bertin6@gmail.com>
2024-04-10 10:37:48 +0200

    add task 1-

---
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Array {
+    public static int elementAt(int[] array, int index) {
+        if (array != null && index >= 0 && index < array.Length) {
+            return array[index];
+        }
+        else {
+            Console.WriteLine("Index out of range");
+            return -1;
+        }
+    }
+}
