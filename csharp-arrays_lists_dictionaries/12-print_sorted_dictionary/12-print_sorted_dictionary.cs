3d5edeb3556d597e41a455001a26abc180a9c7d8
Erkenoss <xavier.bertin6@gmail.com>
2024-04-10 15:18:01 +0200

    add task 12-

---
+++ csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary {
+    public static void PrintSorted(Dictionary<string, string> myDict) {
+        if (myDict != null) {
+            foreach (KeyValuePair<string, string> kvp in myDict.OrderBy(key => key.Key)) {
+                Console.WriteLine("{0}: {1}", kvp.Key, kvp.Value);
+            }
+        }
+    }
+}
