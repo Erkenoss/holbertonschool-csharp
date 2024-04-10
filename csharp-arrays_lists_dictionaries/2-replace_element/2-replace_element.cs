254b46d891d6c76e4ac19fa9531e13dcf512e81d
Erkenoss <xavier.bertin6@gmail.com>
2024-04-10 11:00:29 +0200

    fixed unchanged index 0

--- csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -3,9 +3,8 @@
 class Array {
     public static int[] ReplaceElement(int[] array, int index, int n) {
         if (array != null && index > 0 && index < array.Length) {
-            int[] newArray = array;
-            newArray[index] = n;
-            return newArray;
+            array[index] = n;
+            return array;
         }
         else {
             Console.WriteLine("Index out of range");
