c9464edacc992ea59b07ab0aea7e49e8cad8503a
Erkenoss <xavier.bertin6@gmail.com>
2024-04-10 15:57:51 +0200

    add task 14-

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program {
+    public static void Main() {
+        int [,] rectangularArray = new int[5, 5];
+
+        rectangularArray[2,2] = 1;
+
+        for (int i = 0; i < rectangularArray.GetLength(0); i++) {
+            for (int j = 0; j < rectangularArray.GetLength(1); j++) {
+                if (j < rectangularArray.GetLength(1) - 1) {
+                    Console.Write(rectangularArray[i, j] + " ");
+                }
+                else {
+                    Console.Write(rectangularArray[i, j]);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
