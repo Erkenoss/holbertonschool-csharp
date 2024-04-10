8b2fabe35a1a4623628a52ee1493973585125e2f
Erkenoss <xavier.bertin6@gmail.com>
2024-04-10 14:06:27 +0200

    add task 8-

---
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary {
+    public static int NumberOfKeys(Dictionary<string, string> myDict) {
+        if (myDict != null) {
+            int keyCount = 0;
+            foreach (var pair in myDict) {
+                keyCount++;
+            }
+            return keyCount;
+        }
+        else {
+            return 0;
+        }
+    }
+}
