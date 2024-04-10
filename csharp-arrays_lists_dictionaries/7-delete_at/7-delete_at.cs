c4f6be7a946501d0b886d7ad75dee92a8e0fe2db
Erkenoss <xavier.bertin6@gmail.com>
2024-04-10 13:47:08 +0200

    add task 7-

--- csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 
 class List {
     public static List<int> DeleteAt(List<int> myList, int index) {
-        if (myList != null && index + 1 < myList.Count && index >= 0) {
-            myList.Remove(index + 1);
+        if (myList != null && index < myList.Count && index >= 0) {
+            myList.Remove(index);
         }
         return myList;
     }
