0b823b1def44877bcfdce38fae8dad0205e66b2a
Erkenoss <xavier.bertin6@gmail.com>
2024-04-10 15:32:52 +0200

    add task 13-

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary {
+    public static string BestScore(Dictionary<string, int> myList) {
+        if (myList != null) {
+            int bestScore = 0;
+            string bestStudent = "";
+            foreach (KeyValuePair<string, int> kvp in myList) {
+                if (bestScore < kvp.Value) {
+                    bestScore = kvp.Value;
+                    bestStudent = kvp.Key;
+                }
+            }
+            return bestStudent;
+        }
+        else {
+            return "None";
+        }
+    }
+}
