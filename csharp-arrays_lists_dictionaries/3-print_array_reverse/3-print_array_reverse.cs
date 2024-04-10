a4dfa245c4df579966f8f4f26ed9cbc4c665bbfb
Erkenoss <xavier.bertin6@gmail.com>
2024-04-10 11:12:11 +0200

    add task 3-

---
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Array {
+    public static void Reverse(int[] array) {
+        if (array != null) {
+            int reverse;
+            for (reverse = array.Length - 1; reverse >= 0; reverse--) {
+                if (reverse > 0) {
+                    Console.Write("{0} ", array[reverse]);
+                }
+                else {
+                    Console.Write("{0}", array[reverse]);
+                }
+            }
+        }
+        Console.WriteLine();
+    }
+}
