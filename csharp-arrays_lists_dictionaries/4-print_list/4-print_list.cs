06751e5ce28257d62df975679e9756008376f9b5
Erkenoss <xavier.bertin6@gmail.com>
2024-04-10 11:26:47 +0200

    add task 4-

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List {
+    public static List<int> CreatePrint(int size) {
+        if (size < 0) {
+            Console.Write("Size cannot be negative\n");
+            return null;
+        }
+
+        if (size == 0) {
+            Console.WriteLine();
+            return new List<int>();
+        }
+        List<int> newList = new List<int>();
+        for (int i = 0; i < size; i++) {
+            newList.Add(i);
+            if (i < size - 1) {
+                Console.Write(newList[i] + " ");
+            }
+            else {
+                Console.Write(newList[i] + "\n");
+            }
+        }
+        return newList;
+    }
+}
