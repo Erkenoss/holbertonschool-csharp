d0102bd2ae3c765d7bedae5f6862b3c44e4c5952
Erkenoss <xavier.bertin6@gmail.com>
2024-04-10 15:38:58 +0200

    try fixed BestScore for checker

--- csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 class Dictionary {
     public static string BestScore(Dictionary<string, int> myList) {
-        if (myList != null) {
+        if (myList != null && myList.Count != 0) {
             int bestScore = 0;
             string bestStudent = "";
             foreach (KeyValuePair<string, int> kvp in myList) {
