96c0b0941fd1ca3b4229f9132592984cb7d906e2
Erkenoss <xavier.bertin6@gmail.com>
2024-04-15 13:57:57 +0200

    add task 9-

---
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList {
+    public static int Pop(LinkedList<int> myLList) {
+        if (myLList == null || myLList.Count == 0) {
+            return 0;
+        }
+        int remove = myLList.First.Value;
+        myLList.Remove(myLList.First);
+        return remove;
+    }
+}
