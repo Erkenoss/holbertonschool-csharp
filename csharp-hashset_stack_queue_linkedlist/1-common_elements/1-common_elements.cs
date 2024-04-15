b205e8ebbecef744cf2b72263b06abad13f8d804
Erkenoss <xavier.bertin6@gmail.com>
2024-04-15 10:35:49 +0200

    add task 2-

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List {
+    public static List<int> CommonElements(List<int> list1, List<int> list2) {
+        List<int> myList = new List<int>();
+
+        foreach (int num in list1) {
+            if (list2.Contains(num)) {
+                myList.Add(num);
+            }
+        }
+        return myList;
+    }
+}
