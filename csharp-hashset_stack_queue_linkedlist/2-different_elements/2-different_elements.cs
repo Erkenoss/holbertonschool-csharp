0178029c3c8beb575c603fdfb466d1c3220d6045
Erkenoss <xavier.bertin6@gmail.com>
2024-04-15 11:02:49 +0200

    add task 2-

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List {
+    public static List<int> DifferentElements(List<int> list1, List<int> list2) {
+
+        if (list1 != null && list2 != null) {
+            List<int> myList = new List<int>(list1);
+
+            foreach (int nbr in list2) {
+                if (myList.Contains(nbr)) {
+                    myList.Remove(nbr);
+                }
+                else {
+                    myList.Add(nbr);
+                }
+            }
+            myList.Sort();
+            return myList;
+        }
+       return null;
+    }
+
+}
