fc315d344cfb58f72ef5a158d9bfc9f0fb908197
Erkenoss <xavier.bertin6@gmail.com>
2024-04-15 14:29:42 +0200

    fixed null case

--- csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -5,16 +5,17 @@ class MyQueue {
     public static Queue<string> Info(Queue<string> aQueue, string newItem, string search) {
 
         Console.WriteLine("Number of items: {0}", aQueue.Count);
-        if (aQueue != null) {
-            Console.WriteLine("First item: {0}", aQueue.Peek());
-        }
-        else {
+
+        if (aQueue.Count == 0) {
             Console.WriteLine("Queue is empty");
         }
-
-        aQueue.Enqueue(newItem);
+        else {
+            Console.WriteLine("First item: {0}", aQueue.Peek());
+        }
 
         bool tf = aQueue.Contains(search);
+        aQueue.Enqueue(newItem);
+
         Console.WriteLine("Queue contains \"{0}\": {1}", search, tf);
 
         if (tf) {
