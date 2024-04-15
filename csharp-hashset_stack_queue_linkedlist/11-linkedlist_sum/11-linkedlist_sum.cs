c2e558a2bb35ec0316ec7d35d73da331f4514ac0
Erkenoss <xavier.bertin6@gmail.com>
2024-04-15 14:57:16 +0200

    change syntaxe of add

--- csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
+++ csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -5,7 +5,7 @@ class LList {
     public static int Sum(LinkedList<int> myLList) {
         int sum = 0;
         foreach (int data in myLList) {
-            sum += data;
+            sum = sum + data;
         }
         return sum;
     }
