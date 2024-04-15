413aab4b249f539808311ff2b9e7e1fbe65d7d8e
Erkenoss <xavier.bertin6@gmail.com>
2024-04-15 10:18:06 +0200

    add task 0-

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class List {
+    public static int Sum(List<int> myList) {
+        int sum = 0;
+        List<int> newList = myList.Distinct().ToList();
+
+        if (myList != null) {
+            foreach (int num in newList) {
+                sum += num;
+            }
+        }
+        return sum;
+    }
+}
