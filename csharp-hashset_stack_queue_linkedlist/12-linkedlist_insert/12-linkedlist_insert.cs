3aedf052a72dcbb88d3dc144fb648d6ffeb27f02
Erkenoss <xavier.bertin6@gmail.com>
2024-04-15 15:25:58 +0200

    add task 12-

---
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList {
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n) {
+        LinkedListNode<int> tempNode = myLList.First;
+
+        if (tempNode != null) {
+            if (n < tempNode.Value) {
+                myLList.AddBefore(tempNode, n);
+                return myLList;
+            }
+
+            tempNode = tempNode.next;
+        }
+        return myLList.AddLast(n);
+    }
+}
