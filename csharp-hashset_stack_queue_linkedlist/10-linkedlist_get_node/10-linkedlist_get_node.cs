576efbb5fe7b40f0350400090432c024e524f8fe
Erkenoss <xavier.bertin6@gmail.com>
2024-04-15 14:48:50 +0200

    add task 10-

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList {
+    public static int GetNode(LinkedList<int> myLList, int n) {
+        if (n >= 0 && n < myLList.Count){
+            return myLList.ElementAt(n);
+        }
+        return 0;
+    }
+}
