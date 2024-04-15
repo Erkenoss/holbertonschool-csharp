4fe28ddc85ce1d6cf2688d93dae4f810049d0cf4
Erkenoss <xavier.bertin6@gmail.com>
2024-04-15 15:35:57 +0200

    add task 13-

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList {
+    public static void Delete(LinkedList<int> myLList, int index) {
+        LinkedListNode<int> tempNode = myLList.First;
+
+        if (index <= myLList.Count && index >= 0) {
+            for (int i = 0; i < index; i++) {
+                tempNode = tempNode.Next;
+            }
+            myLList.Remove(tempNode);
+        }
+
+    }
+}
