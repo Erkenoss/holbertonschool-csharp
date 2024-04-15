b2bc04011f266437057f3f70daacc9df5139f8e7
Erkenoss <xavier.bertin6@gmail.com>
2024-04-15 13:18:51 +0200

    add task 5-

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList {
+    public static LinkedList<int> CreatePrint(int size) {
+        LinkedList<int> llist = new LinkedList<int>();
+
+        if (size < 0) {
+            return new LinkedList<int>();
+        }
+        else {
+            for (int i = 0; i < size; i++) {
+                llist.AddLast(i);
+                Console.WriteLine("{0}", i);
+            }
+            return llist;
+        }
+
+    }
+}
